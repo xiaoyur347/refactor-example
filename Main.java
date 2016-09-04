c0c5d04c81f71cf60c0bf9c830e64d27e13d6e4a
xiaoyur347 <xiaoyur347@sina.com>
2016-09-04 21:08:04 +0800

    [test]a small testcase

--- Main.java
+++ Main.java
@@ -9,5 +9,14 @@ class Main {
 		customer.addRental(new Rental(movie3, 20));
 		String result = customer.statement();
 		System.out.println(result);
+		String expect = "Rental Record for Jean\n";
+		expect += "\tmovie_regular\t14.0\n";
+		expect += "\tmovie_new\t45.0\n";
+		expect += "\tmovie_child\t27.0\n";
+		expect += "Amount owed is 86.0\n";
+		expect += "You earned 4 frequent renter points";
+		if (!result.equals(expect)) {
+			System.out.println("unexpected");
+		}
 	}
 }
