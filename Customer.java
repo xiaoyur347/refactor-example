525399977c5a4dd1d5020758da8a9b20c4551352
xiaoyur347 <xiaoyur347@sina.com>
2016-09-04 21:40:56 +0800

    [refactor]replace temp with query

--- Customer.java
+++ Customer.java
@@ -21,9 +21,6 @@ class Customer {
 		while (rentals.hasMoreElements()) {
 			Rental each = (Rental)rentals.nextElement();
 
-			//determine amounts for each line
-			double thisAmount = each.getCharge();
-
 			//add frequent renter points
 			frequentRenterPoints ++;
 			//add bonus for a two day new release rental
@@ -33,8 +30,8 @@ class Customer {
 
 			//show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" +
-				String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+				String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
 		}
 
 		//add footer lines
