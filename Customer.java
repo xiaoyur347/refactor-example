b73a25ee86c5783436b014c787bf707b76e886a1
xiaoyur347 <xiaoyur347@sina.com>
2016-09-04 22:10:21 +0800

    [refactor]replace temp with query

--- Customer.java
+++ Customer.java
@@ -13,24 +13,38 @@ class Customer {
 	public String getName() {
 		return _name;
 	}
-	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
+	public double getTotalCharge() {
+		double result = 0;
 		Enumeration rentals = _rentals.elements();
-		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
 			Rental each = (Rental)rentals.nextElement();
-			frequentRenterPoints += each.getFrequentRenterPoints();
+			result += each.getCharge();
+		}
+		return result;
+	}
+	public int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental)rentals.nextElement();
+			result += each.getFrequentRenterPoints();
+		}
+		return result;
+	}
+	public String statement() {
+		String result = "Rental Record for " + getName() + "\n";
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental)rentals.nextElement();
 
 			//show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" +
 				String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
 		}
 
 		//add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) +
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
 			" frequent renter points";
 		return result;
 	}
